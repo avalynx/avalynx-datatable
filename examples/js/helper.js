010f3af69ec8197140b063d46d99c30a9a34a5ca
jschwind <info@juergen-schwind.de>
2024-02-23 20:34:59 +0100

    helper fixed

--- examples/js/helper.js
+++ examples/js/helper.js
@@ -1,9 +1,11 @@
 document.addEventListener('DOMContentLoaded', () => {
 	const modeToggle = document.getElementById('modeToggle');
-	modeToggle.addEventListener('click', () => {
-		const currentTheme = document.documentElement.getAttribute('data-bs-theme');
-		const newTheme = currentTheme === 'dark' ? 'light' : 'dark';
-		document.documentElement.setAttribute('data-bs-theme', newTheme);
-		modeToggle.textContent = `Toggle to ${newTheme === 'dark' ? 'Light' : 'Dark'} Mode`;
-	});
+	if (modeToggle !== null) {
+		modeToggle.addEventListener('click', () => {
+			const currentTheme = document.documentElement.getAttribute('data-bs-theme');
+			const newTheme = currentTheme === 'dark' ? 'light' : 'dark';
+			document.documentElement.setAttribute('data-bs-theme', newTheme);
+			modeToggle.textContent = `Toggle to ${newTheme === 'dark' ? 'Light' : 'Dark'} Mode`;
+		});
+	}
 });
