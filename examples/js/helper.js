96acb0ff8cd9c1d3d946012890d80074cb8b4088
jschwind <info@juergen-schwind.de>
2024-04-07 12:48:10 +0200

    demo upgrade with code example

--- examples/js/helper.js
+++ examples/js/helper.js
@@ -6,6 +6,24 @@ document.addEventListener('DOMContentLoaded', () => {
 			const newTheme = currentTheme === 'dark' ? 'light' : 'dark';
 			document.documentElement.setAttribute('data-bs-theme', newTheme);
 			modeToggle.textContent = `Toggle to ${newTheme === 'dark' ? 'Light' : 'Dark'} Mode`;
+
+			const hljsTheme = document.getElementById('hljsTheme');
+			if (hljsTheme !== null) {
+				cssTheme = newTheme === 'light' ? 'stackoverflow-light' : 'stackoverflow-dark';
+				hljsTheme.setAttribute('href', `https://cdn.jsdelivr.net/gh/highlightjs/cdn-release@11.9/build/styles/${cssTheme}.min.css`);
+			}
+		});
+	}
+
+	const copyButton = document.getElementById('copyButton');
+	if (copyButton !== null) {
+		copyButton.addEventListener('click', () => {
+			const code = document.getElementById('codeBlock').innerText;
+			navigator.clipboard.writeText(code).then(() => {
+				copyButton.textContent = 'Copied!';
+			}).catch(err => {
+				console.error('Error copying text: ', err);
+			});
 		});
 	}
 });
